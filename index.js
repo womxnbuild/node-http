d608556edccfe51c9851f81a79e0494df87d6685
womxnbuilder <77127555+womxnbuild@users.noreply.github.com>
2021-12-07 23:19:41 -0500

    changes to index.js

--- index.js
+++ index.js
@@ -15,15 +15,38 @@ const server = http.createServer((req, res) => {
         var filePath = path.resolve("./public"+fileUrl);
         const fileExt = path.extname(filePath);
         if (fileExt === '.html'){
+            // this is a callback function if the param file doesn't exists
             fs.exists(filePath, (exists) => {
-
+                if (!exists){
+                    res.statusCode = 404
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end("<html><head>404 Not Found</head>" +
+                        "<body> File Not Found</body></html>");
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
             });
+        } else {
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'text/html');
+            res.end("<html><head><title>404 Not Found</title></head>" +
+                "<body> File not an HTML file</body></html>");
+            return;
         }
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/html');
+        res.end("<html><head><title>404 Not Found</title></head>" +
+            "<body> Request Method Not Found</body></html>");
+        return;
     }
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end("<html><head><title>node-http</title></head><body>"
     + "<h1> Hello world!</h1></body></html>");
+    return;
 });
 
 server.listen(port, hostname, () => {
